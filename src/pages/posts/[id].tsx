9d4e6d97a96ee55b80e431acc06c689cc78cf70c
ueman <p20ym25q@gmail.com>
2021-07-22 21:20:32 +0900

    first time.

---
+++ src/pages/posts/[id].tsx
@@ -0,0 +1,44 @@
+import { GetStaticPaths, GetStaticProps, NextPage } from 'next';
+import { IPost } from '../../model';
+
+type PageProps = {
+  post: IPost;
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const response = await fetch('http://localhost:3004/posts');
+  const posts: IPost[] = await response.json();
+
+  const paths = posts.map((post) => ({
+    params: {
+      id: String(post.id),
+    },
+  }));
+
+  return { paths, fallback: false };
+};
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  const id = ctx.params?.id as string;
+
+  const response = await fetch(`http://localhost:3004/posts/${id}`);
+  const post: IPost = await response.json();
+
+  return {
+    props: {
+      post,
+    },
+  };
+};
+
+const Post: NextPage<PageProps> = ({ post }: PageProps) => {
+  return (
+    <ul>
+      <li>{post.id}</li>
+      <li>{post.title}</li>
+      <li>{post.author}</li>
+    </ul>
+  );
+};
+
+export default Post;

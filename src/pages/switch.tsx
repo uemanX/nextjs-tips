53270844520a233b239c8a6161657c02dd856487
uemanX <yuhei.uema@gmail.com>
2022-08-18 20:33:01 +0900

    first commit.

---
+++ src/pages/switch.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+const CheckboxPage: NextPage = () => {
+  return (
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      Switchコンポーネントを作成する
+    </div>
+  );
+};
+
+export default CheckboxPage;

b28a873dccf785fcaf229fba1b5de9a692d39c05
uemanX <yuhei.uema@gmail.com>
2022-08-20 13:14:27 +0900

    Create textField component.

--- src/component/atom/TextField.tsx
+++ src/component/atom/TextField.tsx
@@ -1,13 +1,88 @@
 import styled from "@emotion/styled";
 
-const Text = styled.input`
-  border: 1px solid #ccc;
+const duration = `200ms`;
+
+const TextFieldWrapper = styled.div`
+  display: inline-block;
+  position: relative;
+
+  // 未入力時
+  & > input {
+    padding: 1em;
+    font-size: 16px;
+    outline: none;
+    border-radius: 4px;
+    border: 2px solid var(--gray);
+  }
+  & > input::placeholder {
+    color: transparent;
+  }
+  & > input + span {
+    position: absolute;
+    left: 0.8em;
+    transform-origin: left top;
+    transition-duration: ${duration};
+    pointer-events: none;
+    padding: 0.2em 0.4em;
+  }
+  & > input + span::after {
+    content: attr(data-placeholder);
+  }
+  & > input + span::after {
+    color: var(--gray);
+  }
+  & > input:placeholder-shown + span {
+    top: 0.8em;
+    background-color: transparent;
+    transform: scale(1);
+  }
+
+  // ホバー時
+  & > input:hover {
+    transition-duration: ${duration};
+    border-color: rgba(0, 0, 0, 0.4);
+  }
+
+  // フォーカス時
+  & > input:focus {
+    border-color: var(--green);
+  }
+  & > input:focus + span::after {
+    color: var(--green);
+  }
+
+  // フォーカス時 or 入力済み
+  & > input + span,
+  & > input:focus + span {
+    top: -0.8em;
+    background-color: white;
+    transform: scale(0.8);
+  }
+
+  // Diasbled
+  & > input:disabled {
+    pointer-events: none;
+  }
 `;
 
-const TextField = () => {
-  return <Text type="text" />;
+type TextFieldProps = {
+  placeholder?: string;
+  disabled?: boolean;
+  value?: string;
+};
+
+const TextField = ({ placeholder, disabled, value }: TextFieldProps) => {
+  return (
+    <TextFieldWrapper>
+      <input
+        type="text"
+        placeholder={placeholder}
+        disabled={disabled}
+        value={value}
+      />
+      <span data-placeholder={placeholder}></span>
+    </TextFieldWrapper>
+  );
 };
 
 export default TextField;
-
-// Google Fontを入れてみる
--- src/pages/textField.tsx
+++ src/pages/textField.tsx
@@ -1,7 +1,14 @@
 import { NextPage } from "next";
+import TextField from "../component/atom/TextField";
 
 const TextFieldPage: NextPage = () => {
-  return <input type="text" />;
+  return (
+    <div style={{ display: "flex", flexDirection: "column", gap: "20px" }}>
+      <TextField placeholder="name" />
+      <TextField placeholder="氏名" />
+      <TextField placeholder="disabled" disabled value="無効" />
+    </div>
+  );
 };
 
 export default TextFieldPage;
